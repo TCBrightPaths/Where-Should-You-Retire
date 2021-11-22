8df9b26fb1b93f45457d30c3d9bdd3aa81989ef0
Tamara Chance <tamara.n.chance@gmail.com>
2021-11-22 16:20:05 -0600

    features 1-5 working but my math is incorrect

--- client/index.js
+++ client/index.js
@@ -1,13 +1,28 @@
 //Destructuring elements from the DOM for first feature
-let calcForm = document.querySelector('.calc-form')
-let calculator = document.querySelector('#calculator')
-let currentSavings = document.querySelector('#current-savings')
-
+let calcBtn = document.querySelector('#calcBtn')
+let form = document.querySelector('.calc-form')
+//Write a function that processes the values taken from the dom
+let calculate = (event) => {
+    event.preventDefault();
+    let s0 = document.querySelector('#current-savings').value
+    let aug = document.querySelector('#monthly-aug').value    
+    let percent = document.querySelector('#roi').value
+    let years = document.querySelector('#years').value
+    let yearlyGrowth = []
+    percent = percent/100 + 1
+    s0 = (s0 + (aug * 12)) * percent
+    while (yearlyGrowth.length < years) {
+        yearlyGrowth.push(s0)
+    }
+    let n = yearlyGrowth[yearlyGrowth.length-1]
+    let result = document.createElement('h2')
+    result.textContent = `In ${years} years you can expect to have a total of $${n} saved for retirement!`
+    form.appendChild(result)
+}
 //Write a function that takes in four parameters(the current savings, the monthly contribution, the expected savings rate, and the time to retirement)
 
-let retirementCalc = (s0, aug, percent, years) => {
-    let s0 = currentSavings.textContent
-    console.log(s0)
+//let retirementCalc = (s0, aug, percent, years) => {
+    //s0 = 
     //create an array to hold yearlyGrowth
     //given a starting s0= currentSavings
     //given a percent = percent/100 + 1
@@ -19,5 +34,6 @@ let retirementCalc = (s0, aug, percent, years) => {
     //let n = the last index of the array with the number of years needed to get a population greater or equal to p
     //console.log(n)
     //return the total amount saved after years
-}
+//}
 
+calcBtn.addEventListener('click', calculate)
