61c884ce31d4d775aa3eed598fad90783ef779c0
Tamara Chance <tamara.n.chance@gmail.com>
2021-11-28 16:28:07 -0600

    v3

--- client/index.js
+++ client/index.js
@@ -1,14 +1,16 @@
 const locationsContainer = document.querySelector('#locations-container')
 const form = document.querySelector('form')
+const res = document.querySelector('.res')
+const listContainer = document.querySelector('#listContainer')
 
 const baseURL = `http://localhost:4000/api/locations`
 
-const locationsCallback = ({ data: locations }) => console.log(locations) //displayLocations(locations)
+const locationsCallback = ({ data: locations }) => displayLocations(locations)
 const errCallback = err => console.log(err)
 
 const getAllLocations = () => axios.get(baseURL).then(locationsCallback).catch(errCallback)
-const retireIncome = (body) => axios.post(baseURL, body).then(locationsCallback).catch(errCallback)
-
+const retireIncome = (body) => axios.post(baseURL, body).then(displayIncome).catch(errCallback)
+const updateItinerary = (name) => axios.put(baseURL, name).then(displayList).catch(errCallback)
 
 function submitHandler(e) {
     e.preventDefault()
@@ -41,8 +43,8 @@ function createLocationCard(location) {
     <p class="city">${location.cityName}</p>
     
       
-        <p class="location-cost">$${location.costLiving}</p>
-   
+        <p class="location-cost">~$${location.costLiving}/yr annual income required for a family of four.</p>
+        <button onclick="updateItinerary(${location.cityName})">Add to List</button>
     `
 
 
@@ -56,5 +58,35 @@ function displayLocations(arr) {
     }
 }
 
+function displayList({data: destinations}) {
+    listContainer.innerHTML = ``
+    for (let i = 0; i < destinations.length; i++) {
+        addList(destinations[i])
+    }
+}
+
+function displayIncome ({data: annualIncome}) {
+    let result = document.createElement("h2")
+    result.classList.add('income-display')
+    let income = document.createElement('span')
+    income.textContent = annualIncome
+    result.appendChild(income)
+    result.textContent = `Based on your savings rate, target retirement date, and a 4% safe withdrawal rate: You can expect an annual income of ${income.textContent}.`
+    res.appendChild(result)
+}
+
+function addList (destination) {
+    let trip = document.createElement('li');
+    let destinationName = document.createElement('span');
+    destinationName.textContent = destination;
+    trip.appendChild(destination);
+    let deleteBtn = document.createElement('button');
+    deleteBtn.textContent = 'X';
+    deleteBtn.addEventListener('click', deleteDestination);
+    trip.appendChild(deleteBtn);
+    listContainer.appendChild(trip);
+}
+
+getAllLocations()
 form.addEventListener('submit', submitHandler)
 
